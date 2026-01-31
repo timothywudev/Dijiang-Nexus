c54037dd24407ebaf5fe734248b44e20ef7f2676
Timothy <wutimothy02@gmail.com>
2026-01-31 00:04:45 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/<REPO>/',
   plugins: [
     react(),
     tsconfigPaths({
