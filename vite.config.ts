402ce989c4b23e95819c4814efb2b06d6ed4716e
Timothy <wutimothy02@gmail.com>
2026-01-31 00:06:29 -0800

    Update deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/Dijiang-Nexus/',
   plugins: [
     react(),
     tsconfigPaths({
